26d35525c30aceee8c23f46f7ccba9d255a0503d
gakuto-higuchi <gakuto.higuchi@gmail.com>
2023-05-14 19:39:30 +0900

    Initial Commit

---
+++ tourmate_swift/tourmate_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  tourmate_swiftApp.swift
+//  tourmate_swift
+//
+//  Created by gakutohiguchi on 2023/05/14.
+//
+
+import SwiftUI
+
+@main
+struct tourmate_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
