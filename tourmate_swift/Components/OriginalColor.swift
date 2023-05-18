af57d3187c54285372ba3d3b5655ecfffe53cdf6
gakuto-higuchi <gakuto.higuchi@gmail.com>
2023-05-19 06:54:21 +0900

    [add] 色を追加する

---
+++ tourmate_swift/Components/OriginalColor.swift
@@ -0,0 +1,17 @@
+//
+//  OriginalColor.swift
+//  tourmate_swift
+//
+//  Created by gakutohiguchi on 2023/05/19.
+//
+
+import Foundation
+
+import SwiftUI
+
+extension Color {
+    static let blackCustom = Color(red: 25 / 255, green: 24 / 255, blue: 37 / 255)
+    static let purpleCustom = Color(red: 134 / 255, green: 93 / 255, blue: 255 / 255)
+    static let pinkCustom = Color(red: 227 / 255, green: 132 / 255, blue: 255 / 255)
+    static let lightPinkCustom = Color(red: 255 / 255, green: 163 / 255, blue: 253 / 255)
+}
