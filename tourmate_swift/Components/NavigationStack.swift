9934d73a3937386f2bcd99f27e60fa22718af429
gakuto-higuchi <gakuto.higuchi@gmail.com>
2023-05-21 16:59:21 +0900

    [add] 画面遷移を一元管理する

--- tourmate_swift/Components/NavigationStack.swift
+++ tourmate_swift/Components/NavigationStack.swift
@@ -7,39 +7,8 @@
 
 import SwiftUI
 // 画面遷移のデータを管理
-class AppEnvironment: ObservableObject {
-    @Published var signUppath: [Route] = []
-}
 
 enum Route: Hashable {
-    case welcome
-    case signUp
-}
-
-struct NavigationModifier: ViewModifier {
-    @Binding var path: [Route]
-
-    @ViewBuilder
-    fileprivate func coordinator(_ route: Route) -> some View {
-        switch route {
-        case .welcome:
-            Welcomeview()
-        case .signUp:
-            SignUpView()
-        }
-    }
-
-    func body(content: Content) -> some View {
-        NavigationStack(path: $path) {
-            content
-                .navigationDestination(for: Route.self) { route in
-                    coordinator(route)
-                }
-        }
-    }
-}
-extension View {
-    func navigation(path: Binding<[Route]>) -> some View {
-        self.modifier(NavigationModifier(path: path))
-    }
+    case Welcome
+    case SignUp
 }
