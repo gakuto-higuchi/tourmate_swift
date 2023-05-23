dcec2d8b85ac361b9f961578cdf9a6e737f6cd01
gakuto-higuchi <gakuto.higuchi@gmail.com>
2023-05-23 14:36:40 +0900

    [fix] FirebaseFirestore用に変更

--- tourmate_swift/tourmate_swiftApp.swift
+++ tourmate_swift/tourmate_swiftApp.swift
@@ -7,7 +7,8 @@
 
 import SwiftUI
 import FirebaseCore
-
+import FirebaseFirestore
+import FirebaseFirestoreSwift
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
