ed6184463cd0e5593fa4327bb3b45b8fe18d178a
gakuto-higuchi <gakuto.higuchi@gmail.com>
2023-05-26 14:58:22 +0900

    [add] map表示をサポートするファイル

--- tourmate_swift/Components/LocationManager.swift
+++ tourmate_swift/Components/LocationManager.swift
@@ -5,4 +5,27 @@
 //  Created by gakutohiguchi on 2023/05/26.
 //
 
-import Foundation
+import MapKit
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    let manager = CLLocationManager()
+    
+    @Published var region = MKCoordinateRegion()
+    
+    override init() {
+        super.init()
+        manager.delegate = self
+        manager.requestWhenInUseAuthorization()
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.distanceFilter = 2
+        manager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        locations.last.map {
+            let center = CLLocationCoordinate2D(latitude: $0.coordinate.latitude, longitude: $0.coordinate.longitude)
+            
+            region = MKCoordinateRegion(center: center, latitudinalMeters: 1000.0, longitudinalMeters: 1000.0)
+        }
+    }
+}
